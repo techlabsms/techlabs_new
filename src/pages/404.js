dc5665f724e10c62da45ffa679425105c1740b54
Richard <info@richardmenning.de>
2019-06-19 16:06:41 +0200

    finished the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import Navbar from "../components/Navbar"
-import {Link} from 'gatsby';
+import { Link } from "gatsby"
 import "../styles/_404.scss"
 
 class NotFound extends Component {
@@ -21,22 +21,36 @@ class NotFound extends Component {
               <p>As a techie you know what it means…</p>
             </div>
           </div>
-          <div className="row mt-5">
-            <div className="col">
-              <h3><span role="img">🤓 </span>Definiton:</h3>
+          <div className="notFound--definition">
+            <div className="row mt-5">
+              <div className="col">
+                <h3>
+                  <span role="img">🤓 </span>Definiton:
+                </h3>
+              </div>
             </div>
-          </div>
-          <div className="row mt-2">
-            <div className="col">
-              The  <span className="hashtag">HTTP 404</span>, 404 Not Found, 404, Page Not Found, or Server <span className="hashtag">Not
-              Found error message</span> is a Hypertext Transfer Protocol (HTTP)
-              standard response code, in computer network communications, to
-              indicate that the browser was able to communicate with a given
-              server, but the server <span className="hashtag">could not find what was requested.</span> The
-              website hosting server will typically generate a "404 Not Found"
-              web page when a user attempts to follow a broken or dead link;
-              hence the 404 error is one of the most recognizable errors
-              encountered on the World Wide Web.
+            <div className="row mt-2">
+              <div className="col notFound--definition">
+                The <span className="hashtag">HTTP 404</span>, 404 Not Found,
+                404, Page Not Found, or Server{" "}
+                <span className="hashtag">Not Found error message</span> is a
+                Hypertext Transfer Protocol (HTTP) standard response code, in
+                computer network communications, to indicate that the browser
+                was able to communicate with a given server, but the server{" "}
+                <span className="hashtag">
+                  could not find what was requested.
+                </span>{" "}
+                The website hosting server will typically generate a "404 Not
+                Found" web page when a user attempts to follow a broken or dead
+                link; hence the 404 error is one of the most recognizable errors
+                encountered on the World Wide Web.
+              </div>
+            </div>
+            <div className="row">
+                <div className="col mt-5">
+                    <Link className="btn btn-primary" to="/">Learn tech</Link>
+                    <Link className="btn btn-secondary" to="/">Return home</Link>
+                </div>
             </div>
           </div>
         </div>
