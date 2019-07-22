2ed8c77ffe08b442569d1c45655258e4385427a7
Richard <info@richardmenning.de>
2019-07-22 09:14:35 +0200

    adds the programm hero

--- src/pages/dataScience.js
+++ src/pages/dataScience.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-import arrowDown from "../assets/arrowDown.png"
+import ProgrammHero from "../components/ProgrammHero"
 
 class dataScience extends Component {
   render() {
@@ -9,34 +9,17 @@ class dataScience extends Component {
       <>
         <Navbar />
         <div className="container-fluid">
-          <section className="dataScience--hero">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-6">
-                  <h1 className="display-5 mt-5 dataScience--heading">
-                    <span className="dataScience--highlighted-first">Data</span>{" "}
-                    <br />
-                    <span className="dataScience--highlighted-second">
-                      Science
-                    </span>
-                  </h1>
-                  <p className="dataScience--intro mb-5">
-                    TechLabs is the community that enables you to become a
-                    Digital Entrepreneur. Exciting meetups, talks, hackathons,
-                    social events, workshops and much more await you. Discover
-                    TechLabs now. TechLabs is the community that enables you to
-                    become a Digital Entrepreneur. Exciting meetups, talks,
-                    hackathons, social events, workshops and much more await
-                    you. Discover TechLabs now.
-                  </p>
-                  <a className="locations--hero-btn" href="#locations-techlabs">
-                    <img src={arrowDown} alt="locations"/>
-                  </a>
-                </div>
-                <div className="col-md-6" />
-              </div>
-            </div>
-          </section>
+          <ProgrammHero
+            headingFirst="Data"
+            headingSecond=" Science"
+            intro=" TechLabs is the community that enables you to become a Digital
+                Entrepreneur. Exciting meetups, talks, hackathons, social
+                events, workshops and much more await you. Discover TechLabs
+                now. TechLabs is the community that enables you to become a
+                Digital Entrepreneur. Exciting meetups, talks, hackathons,
+                social events, workshops and much more await you. Discover
+                TechLabs now."
+          />
         </div>
         <Footer />
       </>
