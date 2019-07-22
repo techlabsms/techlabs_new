d398780c45aa8d0e09ebae2768ce37956110d7e6
Richard <info@richardmenning.de>
2019-07-22 10:42:31 +0200

    add component

--- src/pages/dataScience.js
+++ src/pages/dataScience.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import ProgrammHero from "../components/ProgrammHero"
+import WhatYouWillLearn from '../components/WhatYouWillLearn'
 
 class dataScience extends Component {
   render() {
@@ -12,6 +13,15 @@ class dataScience extends Component {
           <ProgrammHero
             headingFirst="Data"
             headingSecond=" Science"
+          />
+        </div>
+        <div className="container">
+          <WhatYouWillLearn 
+            intro="Data Science is a multidisciplinary domain which is in it’s core the task of finding valuable insights from data"
+            first="How to apply machine learning models to solve problems like churn prediction or customer segmentation"
+            second="Exploration of raw data to gain useful insights"
+            third="General programming knowledge in R or Python"
+            fourth="How to present your results in a structured and visually appealing way"
           />
         </div>
         <Footer />
