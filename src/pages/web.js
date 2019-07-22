3c3a566518dfc956810defed73757f34cf5a76d5
Richard <info@richardmenning.de>
2019-07-22 09:33:52 +0200

    add programm hero

--- src/pages/web.js
+++ src/pages/web.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
-import Navbar from '../components/Navbar';
+import React, { Component } from "react"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+import ProgrammHero from "../components/ProgrammHero"
 
 class web extends Component {
-    render() {
-        return (
-            <div>
-            <Navbar/>
-                web
-            </div>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className="container-fluid">
+          <ProgrammHero
+            headingFirst="Web"
+            headingSecond=" Development"
+          />
+        </div>
+        <Footer />
+      </>
+    )
+  }
 }
 
-export default web;
+export default web
