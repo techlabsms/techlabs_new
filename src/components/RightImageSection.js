0a742e25c1ccc360b2f92c7cbb6cefa41908cda7
Richard <info@richardmenning.de>
2019-07-22 12:01:01 +0200

    responsive changes

--- src/components/RightImageSection.js
+++ src/components/RightImageSection.js
@@ -11,11 +11,11 @@ const RightImageSection = ({ heading, subheading, text, image }) => {
         </div>
       </div>
       <div className="row mt-5">
-        <div className="col-6">
+        <div className="col-md-7 pr-5 order-2 order-md-1">
           <p className="basicSection--text">{text}</p>
         </div>
-        <div className="col-6 text-center">
-          <img src={image} alt="" />
+        <div className="col-md-5 text-center  order-1 order-md-1 mb-3">
+          <img src={image} alt="" className="w-100 mb-3"/>
         </div>
       </div>
     </>
