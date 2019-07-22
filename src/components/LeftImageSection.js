07db1f1b6d2721734658b8b2b94831157264704b
Richard <info@richardmenning.de>
2019-07-22 12:00:50 +0200

    add left image section

--- src/components/LeftImageSection.js
+++ src/components/LeftImageSection.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react"
 
-const LeftImageSection = () => {
-    return (
-        <>
-            
-        </>
-    );
-};
+const LeftImageSection = ({ heading, subheading, text, image }) => {
+  return (
+    <>
+      <div className="row mt-5">
+        <div className="col">
+          <h2>{heading}</h2>
+          <div className="section-divider" />
+          <p className="basicSection--sub">{subheading}</p>
+        </div>
+      </div>
+      <div className="row mt-5">
+        <div className="col-md-5 text-center mb-3">
+          <img src={image} alt="" className="w-100"/>
+        </div>
+        <div className="col-md-7">
+          <p className="basicSection--text">{text}</p>
+        </div>
+      </div>
+    </>
+  )
+}
 
-export default LeftImageSection;
+export default LeftImageSection
