036545ed98755844df77713f7682c11e473cbbed
Richard <info@richardmenning.de>
2019-07-22 10:42:21 +0200

    finish component

---
+++ src/components/WhatYouWillLearn.js
@@ -0,0 +1,79 @@
+import React from "react"
+
+const WhatYouWillLearn = ({ intro, first, second, third, fourth }) => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col">
+          <h2>What you will learn</h2>
+          <div className="section-divider" />
+          <p>
+            {intro}
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="learn--card">
+            <div className="row mt-3">
+              <div className="col-1">
+                <div className="learn--color" />
+              </div>
+              <div className="col-11 mt-3">
+                <p>
+                {first}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col">
+          <div className="learn--card">
+            <div className="row">
+              <div className="col-1">
+                <div className="learn--color" />
+              </div>
+              <div className="col-11 mt-3">
+                <p>{second}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col">
+          <div className="learn--card">
+            <div className="row">
+              <div className="col-1">
+                <div className="learn--color" />
+              </div>
+              <div className="col-11 mt-3">
+                <p>{third}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col">
+          <div className="learn--card">
+            <div className="row">
+              <div className="col-1">
+                <div className="learn--color" />
+              </div>
+              <div className="col-11 mt-3">
+                <p>
+                  {fourth}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WhatYouWillLearn
