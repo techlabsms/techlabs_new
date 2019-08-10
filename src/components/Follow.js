2714dbb45f867b3c72eeda50e24bd60e8164d0df
Richard <info@richardmenning.de>
2019-08-10 16:22:59 +0200

    made links dynamic

--- src/components/Follow.js
+++ src/components/Follow.js
@@ -4,7 +4,14 @@ import instagram from "../assets/instagram.png"
 import linkedin from "../assets/linkedin.png"
 import medium from "../assets/medium.png"
 
-const Follow = ({ heading, subheading }) => {
+const Follow = ({
+  heading,
+  subheading,
+  facebookLink,
+  instagramLink,
+  linkedInLink,
+  mediumLink,
+}) => {
   return (
     <>
       <div className="row mt-5">
@@ -16,26 +23,42 @@ const Follow = ({ heading, subheading }) => {
       </div>
       <div className="row my-4">
         <div className="col-md-2" />
-        <div className="col-md-2 d-flex justify-content-center">
-          <div className="follow--circle text-center">
-            <img src={facebook} alt="" className="follow--facebook" />
+        {facebookLink && (
+          <div className="col-md-2 d-flex justify-content-center">
+            <a href={facebookLink}>
+              <div className="follow--circle text-center">
+                <img src={facebook} alt="" className="follow--facebook" />
+              </div>
+            </a>
           </div>
-        </div>
-        <div className="col-md-2 d-flex justify-content-center">
-          <div className="follow--circle text-center">
-            <img src={instagram} alt="" className="follow--img" />
+        )}
+        {instagramLink && (
+          <div className="col-md-2 d-flex justify-content-center">
+            <a href={instagramLink}>
+              <div className="follow--circle text-center">
+                <img src={instagram} alt="" className="follow--img" />
+              </div>
+            </a>
           </div>
-        </div>
-        <div className="col-md-2 d-flex justify-content-center">
-          <div className="follow--circle text-center">
-            <img src={linkedin} alt="" className="follow--img" />
+        )}
+        {linkedInLink && (
+          <div className="col-md-2 d-flex justify-content-center">
+            <a href={linkedInLink}>
+              <div className="follow--circle text-center">
+                <img src={linkedin} alt="" className="follow--img" />
+              </div>
+            </a>
           </div>
-        </div>
-        <div className="col-md-2 d-flex justify-content-center">
-          <div className="follow--circle text-center">
-            <img src={medium} alt="" className="follow--img" />
+        )}
+        {mediumLink && (
+          <div className="col-md-2 d-flex justify-content-center">
+            <a href={mediumLink}>
+              <div className="follow--circle text-center">
+                <img src={medium} alt="" className="follow--img" />
+              </div>
+            </a>
           </div>
-        </div>
+        )}
 
         <div className="col-md-2" />
       </div>
