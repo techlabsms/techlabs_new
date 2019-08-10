28cc03da76f54f695eef0ddc6f603d6e9f4ced9d
Richard <info@richardmenning.de>
2019-08-10 16:23:23 +0200

    new component with different heading

---
+++ src/components/RightImageSectionHeading.js
@@ -0,0 +1,36 @@
+import React from "react"
+
+const RightImageSectionHeading = ({
+  heading,
+  subheading,
+  text,
+  image,
+  hasButton,
+  buttonText,
+  buttonLink,
+}) => {
+  return (
+    <>
+      <div className="row mt-5">
+        <div className="col-md-6 order-2 order-md-1">
+          <h2>{heading}</h2>
+          <div className="section-divider" />
+          <p className="basicSection--sub">{subheading}</p>
+          <div className="basicSection--text">{text}</div>
+          {hasButton && (
+            <div className="row mt-5 ml-2">
+              <a className="btn btn-primary d-inline" href={buttonLink}>
+                {buttonText}
+              </a>
+            </div>
+          )}
+        </div>
+        <div className="col-md-6 text-center  order-1 order-md-1 mb-3">
+          <img src={image} alt="" className="w-100 mb-3" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default RightImageSectionHeading
