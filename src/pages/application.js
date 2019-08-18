19a47f55910a8565c2f1140c8c7d6925b2d0231a
Richard <info@richardmenning.de>
2019-08-18 11:40:35 +0200

    Add application process

--- src/pages/application.js
+++ src/pages/application.js
@@ -2,7 +2,9 @@ import React, { Component } from "react"
 import Navbar from "../components/Navbar"
 import ApplicationHero from "../components/ApplicationHero"
 import ChooseCity from "../components/ChooseCity"
+import Requirements from "../components/Requirements"
 import applicationHero from "../assets/application.png"
+import ApplicationProcess from "../components/ApplicationProcess"
 
 class application extends Component {
   render() {
@@ -14,7 +16,18 @@ class application extends Component {
           headingFirst="Application"
           intro="Do you want to learn state-of-the-art tech? Do you want to dive into a thriving community? Apply now for the TechLabs Digital Shaper Program. "
         />
-        <ChooseCity/>
+        <ChooseCity
+          heading="Choose yoour city"
+          subheading="Where do you want to study?"
+        />
+        <Requirements
+          heading="Application Requirements"
+          subheading="What we are looking for in an applicant?"
+        />
+        <ApplicationProcess
+          heading="Application Process"
+          subheading="What are the steps for a successful application? "
+        />
       </>
     )
   }
