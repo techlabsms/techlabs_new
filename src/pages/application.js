973116633ae152ce8e58ab5a821018dab159ae89
Richard <info@richardmenning.de>
2019-08-18 19:55:20 +0200

    add the missing sections

--- src/pages/application.js
+++ src/pages/application.js
@@ -5,6 +5,10 @@ import ChooseCity from "../components/ChooseCity"
 import Requirements from "../components/Requirements"
 import applicationHero from "../assets/application.png"
 import ApplicationProcess from "../components/ApplicationProcess"
+import CallToAction from "../components/CallToAction"
+import Faq from "../components/Faq"
+import FaqQuestion from "../components/FaqQuestion"
+import Footer from '../components/Footer'
 import Dates from "../components/Dates"
 
 class application extends Component {
@@ -29,10 +33,25 @@ class application extends Component {
           heading="Application Process"
           subheading="What are the steps for a successful application? "
         />
-        <Dates
-          heading="Important Dates"
-          subheading="What is happening when?"
-        />
+        <Dates heading="Important Dates" subheading="What is happening when?" />
+        <div className="container">
+          <CallToAction />
+          <Faq>
+            <FaqQuestion
+              question="How do I apply for the Digital Shaper Program?"
+              answer="This can be done directly via our application form for the respective location."
+            />
+            <FaqQuestion
+              question="What should I write in my application to be accepted?"
+              answer="At TechLabs we want to get to know you and your motivation better. Thats why its generally true that there is no right or wrong answer to the questions. Please only make sure that your answer really refers to the question. Unfortunately, we cannot evaluate important aspects that do not relate to the question."
+            />
+            <FaqQuestion
+              question="How can I imagine the time required?"
+              answer="In general, learning is an individual process. Nevertheless, we recommend that you work regularly on your curriculum and allow for about 5 hours per week. Please also bear in mind that the project phase can mean increased coordination effort with your project team. In any case, the invested time will be worth it."
+            />
+          </Faq>
+        </div>
+        <Footer/>
       </>
     )
   }
