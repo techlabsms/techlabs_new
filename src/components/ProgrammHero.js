30a17262090fcca240580b8f0dc1104001ca1a27
Richard <info@richardmenning.de>
2019-09-06 12:59:57 +0200

    add card to mobile

--- src/components/ProgrammHero.js
+++ src/components/ProgrammHero.js
@@ -20,7 +20,7 @@ const ProgrammHero = ({
       >
         <div className="container h-100">
           <div className="row">
-            <div className="col-md-6">
+            <div className="col-md-12 col-lg-6">
               <h1 className="display-5 mt-5 programmHero--heading">
                 <span className="programmHero--highlighted-first">
                   {headingFirst}
@@ -30,79 +30,132 @@ const ProgrammHero = ({
                   {headingSecond}
                 </span>
               </h1>
-              <p className="programmHero--intro mb-5">
-                {intro}
-              </p>
+
+              <p className="programmHero--intro mb-5">{intro}</p>
             </div>
             {showCard && (
-              <div className="col-md-6 d-none d-sm-none d-md-block">
-                <div className="card programmHero--card">
-                  <h2 className="programmHero--card-heading text-center">
-                    {headingFirst} {headingSecond}
-                  </h2>
-                  <div className="row pl-5">
-                    <div className="col">
-                      <h4>Time</h4>
-                      <div className="section-divider" />
-                      <div className="row">
-                        <div className="col-2">
-                          <img
-                            src={clock}
-                            alt="clock"
-                            className="align-middle"
-                          />
-                        </div>
-                        <div className="col-10">
-                          <p>
-                            6 Months Program
-                            <br />
-                            10 Hours per Week
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row pl-5">
-                    <div className="col">
-                      <h4>Certificate</h4>
-                      <div className="section-divider" />
-                      <div className="row">
-                        <div className="col-2">
-                          <img
-                            src={diploma}
-                            alt="clock"
-                            className="align-middle"
-                          />
-                        </div>
-                        <div className="col-10">
-                          <p>
-                            Get a graduation certificate <br /> by submitting a
-                            project
-                          </p>
+              <>
+                <div className="col-md-6 d-none d-lg-block">
+                  <div className="card programmHero--card">
+                    <h2 className="programmHero--card-heading text-center">
+                      {headingFirst} {headingSecond}
+                    </h2>
+                    <div className="row pl-5">
+                      <div className="col">
+                        <h4>Time</h4>
+                        <div className="section-divider" />
+                        <div className="row">
+                          <div className="col-2">
+                            <img
+                              src={clock}
+                              alt="clock"
+                              className="align-middle"
+                            />
+                          </div>
+                          <div className="col-10">
+                            <p>
+                              6 Months Program
+                              <br />
+                              10 Hours per Week
+                            </p>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="row pl-5">
-                    <div className="col">
-                      <h4>Participants</h4>
-                      <div className="section-divider" />
-                      <div className="row">
-                        <div className="col-2">
-                          <img
-                            src={profile}
-                            alt="clock"
-                            className="align-middle"
-                          />
+                    <div className="row pl-5">
+                      <div className="col">
+                        <h4>Certificate</h4>
+                        <div className="section-divider" />
+                        <div className="row">
+                          <div className="col-2">
+                            <img
+                              src={diploma}
+                              alt="clock"
+                              className="align-middle"
+                            />
+                          </div>
+                          <div className="col-10">
+                            <p>
+                              Get a graduation certificate <br /> by submitting
+                              a project
+                            </p>
+                          </div>
                         </div>
-                        <div className="col-10">
-                          <p>Over 250 grads</p>
+                      </div>
+                    </div>
+                    <div className="row pl-5">
+                      <div className="col">
+                        <h4>Participants</h4>
+                        <div className="section-divider" />
+                        <div className="row">
+                          <div className="col-2">
+                            <img
+                              src={profile}
+                              alt="clock"
+                              className="align-middle"
+                            />
+                          </div>
+                          <div className="col-10">
+                            <p>Over 250 grads</p>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
+                <div className="col-lg-12 d-block d-lg-none">
+                  <div className="card programmHero--card">
+                    <h2 className="programmHero--card-heading-small text-center">
+                      {headingFirst} {headingSecond}
+                    </h2>
+                    <div className="row mt-2">
+                      <div className="col-2">
+                        <img src={clock} alt="clock" width="30" />
+                      </div>
+                      <div className="col-4">
+                        <h2 className="programmHero--card-heading-second mt-2">
+                          Time
+                        </h2>
+                      </div>
+                      <div className="col-6">
+                        <p>
+                          6 Months Program
+                          <br />
+                          10 Hours per Week
+                        </p>
+                      </div>
+                    </div>
+                    <div className="row mt-2">
+                      <div className="col-2">
+                        <img src={diploma} alt="clock" width="30" />
+                      </div>
+                      <div className="col-4">
+                        <h2 className="programmHero--card-heading-second mt-2">
+                          Certificate
+                        </h2>
+                      </div>
+                      <div className="col-6">
+                        <p>
+                          Get a graduation certificate by submitting a project
+                        </p>
+                      </div>
+                    </div>
+                    <div className="row mt-2">
+                      <div className="col-2">
+                        <img src={profile} alt="clock" width="30" />
+                      </div>
+                      <div className="col-4">
+                        <h2 className="programmHero--card-heading-second mt-2">
+                          Participants
+                        </h2>
+                      </div>
+                      <div className="col-6">
+                        <p className="mt-2">Over 250 grads</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
             )}
           </div>
         </div>
