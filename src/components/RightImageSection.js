67111070282864a58a1466c099da5eae8882f044
Richard <info@richardmenning.de>
2019-10-03 23:14:13 +0200

    add float possibilty

--- src/components/RightImageSection.js
+++ src/components/RightImageSection.js
@@ -8,6 +8,9 @@ const RightImageSection = ({
   hasButton,
   buttonText,
   buttonLink,
+  float,
+  leftPartSize,
+  rightPartSize,
 }) => {
   return (
     <div className="container my-5 py-5 h-100">
@@ -18,21 +21,39 @@ const RightImageSection = ({
           <p className="basicSection--sub">{subheading}</p>
         </div>
       </div>
-      <div className="row mt-5">
-        <div className="col-md-7 order-2 order-md-1">
+      {float && (
+        <div className="d-none d-md-block mt-5 row">
+          <div className="col">
+            <img
+              src={image}
+              alt=""
+              className="w-50 img-fluid float-right pl-3 pb-2 pt-2 ml-3"
+            />
+            <p className="basicSection--text">{text}</p>
+          </div>
+          {/* Stops the floating - resolved positioning issues*/}
+          <div
+            style={{
+              clear: "both",
+            }}
+          />
+        </div>
+      )}
+      <div className={float ? "row mt-5 d-md-none" : "row mt-5"}>
+        <div className={`col-md-${leftPartSize || 7} order-2 order-md-1`}>
           <p className="basicSection--text">{text}</p>
           {hasButton && (
             <div className="row mt-5 ml-2">
-              <a
-                className="btn btn-primary d-inline"
-                href={buttonLink}
-              >
+              <a className="btn btn-primary d-inline" href={buttonLink}>
                 {buttonText}
               </a>
             </div>
           )}
         </div>
-        <div className="col-md-5 text-center  order-1 order-md-1 mb-3">
+        <div
+          className={`col-md-${rightPartSize ||
+            5} text-center  order-1 order-md-1 mb-3`}
+        >
           <img src={image} alt="" className="w-100 mb-3" />
         </div>
       </div>
