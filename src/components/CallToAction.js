12fa6e1b9353ad881f70a31c4bc1f7aa445ef954
Richard <info@richardmenning.de>
2019-10-07 23:31:22 +0200

    styling of the call to action

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -8,11 +8,16 @@ const CallToAction = () => {
     <section className="callToAction">
       <div className="row">
         <div className="col-3 col-md-1">
-          <img src={ArrowsLeft} alt="arrow" className="float-right  callToAction--arrow" />
+          <img
+            src={ArrowsLeft}
+            alt="arrow"
+            className="float-right  callToAction--arrow"
+          />
         </div>
-        <div className="col-6 col-md-6">
+        <div className="col-6 col-md-4">
           <h1 className="foundYourOwn--last text-center text-md-left  callToAction--text">
-            You can´t wait to acquire the most {" "}<span className="foundYourOwn--cta">in demand tech skills?</span>
+            You can´t wait to acquire the most{" "}
+            <span className="foundYourOwn--cta"> in demand tech skills?</span>
           </h1>
         </div>
         <div className="col-3 col-md-5">
@@ -23,16 +28,20 @@ const CallToAction = () => {
           />
         </div>
       </div>
-      <div className="row mt-5">
-        <div className="col-3">
-          <img src={ArrowsLeft} alt="arrow" className="float-right callToAction--arrow" />
+      <div className="row mt-5 py-5">
+        <div className="col-3 col-md-5">
+          <img
+            src={ArrowsLeft}
+            alt="arrow"
+            className="float-right callToAction--arrow"
+          />
         </div>
-        <div className="col-6">
+        <div className="col-6 col-md-4">
           <h1 className="foundYourOwn--last text-center callToAction--text">
             Are you ready <span className="foundYourOwn--cta">to learn?</span>
           </h1>
         </div>
-        <div className="col-3">
+        <div className="col-3 col-md-1">
           <img
             src={ArrowsRight}
             alt="arrow"
@@ -41,10 +50,10 @@ const CallToAction = () => {
         </div>
       </div>
 
-      <div className="container mt-5 py-5">
+      <div className="container my-5 pb-5">
         <div className="row text-center mt-5">
-          <div className="col">
-            <Link className="btn btn-primary d-inline" to="/apply">
+          <div className="col-12">
+            <Link className="btn btn-primary" to="/apply">
               Apply now
             </Link>
           </div>
