942a1484cfb9326563d3f928a26907348552f471
Richard <info@richardmenning.de>
2019-10-21 17:30:23 +0200

    new styling and different text

--- src/components/PartnerLogos.js
+++ src/components/PartnerLogos.js
@@ -1,26 +1,35 @@
 import React from "react"
-import Container from './smallComponents/Container'
+import Container from "./smallComponents/Container"
 
 const PartnerLogos = ({ logoOne, logoTwo, logoThree, logoFour }) => {
   return (
     <Container>
       <div className="row my-5 d-flex h-100 partnerLogos">
-        <div className="col-md-4 col-12 text-center text-md-left justify-content-center align-self-center">
+        <div className="col-md-3 col-12 text-center text-md-left justify-content-center align-self-center">
           <p className="partnerLogos--text my-5">
-            trusted by our <br /> renowned partners
+            Some Partners - <br />
+            we worked with
           </p>
         </div>
         <div className="col-md-2 col-6 my-5 text-center  justify-content-center align-self-center">
-          <img src={logoOne} alt="partner logo" className="partnerLogos--img"/>
+          <img src={logoOne} alt="partner logo" className="partnerLogos--img img-fluid" />
         </div>
-        <div className="col-md-2 col-6  my-5 text-center justify-content-center align-self-center">
-          <img src={logoTwo} alt="partner logo" className="partnerLogos--img"/>
-        </div>
-        <div className="col-md-2 col-6 my-5 text-center  justify-content-center align-self-center">
-          <img src={logoThree} alt="partner logo" className="partnerLogos--img"/>
+        <div className="col-md-2 col-6 my-5   text-center justify-content-center align-self-center">
+          <img src={logoTwo} alt="partner logo" className="partnerLogos--img img-fluid" />
         </div>
         <div className="col-md-2 col-6 my-5 text-center  justify-content-center align-self-center">
-          <img src={logoFour} alt="partner logo" className="partnerLogos--img"/>
+          <img
+            src={logoThree}
+            alt="partner logo"
+            className="partnerLogos--img img-fluid"
+          />
+        </div>
+        <div className="col-md-2 col-6 my-5 text-center  justify-content-center align-self-center">
+          <img
+            src={logoFour}
+            alt="partner logo"
+            className="partnerLogos--img img-fluid"
+          />
         </div>
       </div>
     </Container>
