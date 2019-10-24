5c1f6c3d37ca2876f8eeb98cdc2063e58f842240
Richard <info@richardmenning.de>
2019-10-24 22:44:21 +0200

    finish calendar - quick fix

---
+++ src/components/DatesCalendar.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Container from "./smallComponents/Container"
+import Heading from "./smallComponents/Heading"
+import { Link } from "gatsby"
+import dayjs from "dayjs"
+
+const DatesCalendar = ({ eventsPage, city}) => {
+  return (
+    <Container>
+        <Heading
+            heading="Important Dates"
+            subheading={`Get an overview all Events and Deadlines in ${city}`}
+        />
+        <a href={eventsPage} className="btn btn-primary noDec text-light">
+            See now 
+        </a>
+    </Container>
+  )
+}
+
+export default DatesCalendar
