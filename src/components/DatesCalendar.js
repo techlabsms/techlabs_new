04cb6088a8125a3b08a10c48fc2b55f27728acba
Richard <info@richardmenning.de>
2019-11-10 20:59:33 +0100

    calendar for the next monts

--- src/components/DatesCalendar.js
+++ src/components/DatesCalendar.js
@@ -1,19 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import Container from "./smallComponents/Container"
 import Heading from "./smallComponents/Heading"
+import dayjs from "dayjs"
 
-const DatesCalendar = ({ eventsPage, city}) => {
+const DatesCalendar = ({ eventsPage, city }) => {
+  const month = getNextFiveMonth()
   return (
     <Container>
-        <Heading
-            heading="Important Dates"
-            subheading={`Get an overview all Events and Deadlines in ${city}`}
-        />
-        <a href={eventsPage} className="btn btn-primary noDec text-light">
-            See now 
-        </a>
+      <Heading
+        heading="Important Dates"
+        subheading={`Get an overview all Events and Deadlines in ${city}`}
+      />
+      <div className="card">
+        <div className="row">
+          {month.map(date => (
+            <div className="col datesCalendar--dates" key={date.month()}>
+              <h3>{date.format("MMMM")}</h3>
+            </div>
+          ))}
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <div>
+                <p className="datesCalendar--day">16</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Container>
   )
 }
+
+const getNextFiveMonth = () => {
+  const dates = "01234".split("").map(day => {
+    console.log(dayjs().add(day, "month"))
+    return dayjs().add(day, "month")
+  })
+  return dates
+}
+
+const getEventsFromMonth = month => {}
 
 export default DatesCalendar
