101effc103a9b699a3b59e30956c710e1e4d01e6
Richard <richardmenning@googlemail.com>
2020-03-18 22:09:05 +0100

    add track availability

--- src/enums/CountryEnum.js
+++ src/enums/CountryEnum.js
@@ -1,88 +1,159 @@
 export const CountryEnum = {
-    COUNTRY: 'location.country',
-    GERMANY: 'location.country.germany',
-    SPAIN: 'location.country.spain',
-    DENMARK: 'location.country.denmark',
-    BRAZIL: 'location.country.brazil',
-    COLOMBIA: 'location.country.colombia'
-};
+  COUNTRY: "location.country",
+  GERMANY: "location.country.germany",
+  SPAIN: "location.country.spain",
+  DENMARK: "location.country.denmark",
+  BRAZIL: "location.country.brazil",
+  COLOMBIA: "location.country.colombia",
+}
 
 export const CityEnum = {
-    MUENSTER: 'location.muenster',
-    BERLIN: 'location.berlin',
-    COPENHAGEN: 'location.copenhagen',
-    MEDELLIN: 'location.medellin',
-    CURITIBA: 'location.curitiba',
-    BARCELONA: 'location.barcelona',
-    AACHEN: 'location.aachen',
-};
+  MUENSTER: "location.muenster",
+  BERLIN: "location.berlin",
+  COPENHAGEN: "location.copenhagen",
+  MEDELLIN: "location.medellin",
+  CURITIBA: "location.curitiba",
+  BARCELONA: "location.barcelona",
+  AACHEN: "location.aachen",
+}
 
 export const CityCountryEnum = {
-    CITY: [{value: 'location.city'}],
-    GERMANY: [{value: 'location.muenster', applicationStart: '20200315', applicationEnd: '20200412'},
-        {value: 'location.berlin', applicationStart: '20200301', applicationEnd: '20200328'}, {
-            value: 'location.aachen', applicationStart: null, applicationEnd: null
-        }],
-    SPAIN: [{
-        value: 'location.barcelona', applicationStart: null, applicationEnd: null
-    }],
-    DENMARK: [{
-        value: 'location.copenhagen', applicationStart: null, applicationEnd: null
-    }],
-    BRAZIL: [{
-        value: 'location.curitiba', applicationStart: '20200815', applicationEnd: '20200826'
-    }],
-    COLOMBIA: [{
-        value: 'location.medellin', applicationStart: null, applicationEnd: null
-    }]
-};
+  CITY: [{ value: "location.city" }],
+  GERMANY: [
+    {
+      value: "location.muenster",
+      applicationStart: "20200315",
+      applicationEnd: "20200412",
+      availableTracks: {
+        ai: true,
+        web: true,
+        dataScience: true,
+        ux: false,
+      },
+    },
+    {
+      value: "location.berlin",
+      applicationStart: "20200301",
+      applicationEnd: "20200328",
+      availableTracks: {
+        ai: true,
+        web: true,
+        dataScience: true,
+        ux: false,
+      },
+    },
+    {
+      value: "location.aachen",
+      applicationStart: null,
+      applicationEnd: null,
+      availableTracks: {
+        ai: true,
+        web: true,
+        dataScience: true,
+        ux: false,
+      },
+    },
+  ],
+  SPAIN: [
+    {
+      value: "location.barcelona",
+      applicationStart: null,
+      applicationEnd: null,
+      availableTracks: {
+        ai: true,
+        web: true,
+        dataScience: true,
+        ux: false,
+      },
+    },
+  ],
+  DENMARK: [
+    {
+      value: "location.copenhagen",
+      applicationStart: null,
+      applicationEnd: null,
+      availableTracks: {
+        ai: true,
+        web: true,
+        dataScience: true,
+        ux: false,
+      },
+    },
+  ],
+  BRAZIL: [
+    {
+      value: "location.curitiba",
+      applicationStart: "20200815",
+      applicationEnd: "20200826",
+      availableTracks: {
+        ai: true,
+        web: true,
+        dataScience: true,
+        ux: false,
+      },
+    },
+  ],
+  COLOMBIA: [
+    {
+      value: "location.medellin",
+      applicationStart: null,
+      applicationEnd: null,
+      availableTracks: {
+        ai: true,
+        web: true,
+        dataScience: true,
+        ux: false,
+      },
+    },
+  ],
+}
 
 export function getCityValues(city) {
-    switch (city) {
-        case null:
-            return;
+  switch (city) {
+    case null:
+      return
 
-        case CityEnum.BARCELONA:
-            return CityCountryEnum.SPAIN[0];
+    case CityEnum.BARCELONA:
+      return CityCountryEnum.SPAIN[0]
 
-        case CityEnum.BERLIN:
-            return CityCountryEnum.GERMANY[1];
+    case CityEnum.BERLIN:
+      return CityCountryEnum.GERMANY[1]
 
-        case CityEnum.AACHEN:
-            return CityCountryEnum.GERMANY[2];
+    case CityEnum.AACHEN:
+      return CityCountryEnum.GERMANY[2]
 
-        case CityEnum.COPENHAGEN:
-            return CityCountryEnum.DENMARK[0];
+    case CityEnum.COPENHAGEN:
+      return CityCountryEnum.DENMARK[0]
 
-        case CityEnum.CURITIBA:
-            return CityCountryEnum.BRAZIL[0];
+    case CityEnum.CURITIBA:
+      return CityCountryEnum.BRAZIL[0]
 
-        case CityEnum.MUENSTER:
-            return CityCountryEnum.GERMANY[0];
+    case CityEnum.MUENSTER:
+      return CityCountryEnum.GERMANY[0]
 
-        case CityEnum.MEDELLIN:
-            return CityCountryEnum.COLOMBIA[0];
-    }
+    case CityEnum.MEDELLIN:
+      return CityCountryEnum.COLOMBIA[0]
+  }
 }
 
 export function getCity(country) {
-    switch (country) {
-        case CountryEnum.COUNTRY:
-            return CityCountryEnum.CITY;
+  switch (country) {
+    case CountryEnum.COUNTRY:
+      return CityCountryEnum.CITY
 
-        case CountryEnum.GERMANY:
-            return CityCountryEnum.GERMANY;
+    case CountryEnum.GERMANY:
+      return CityCountryEnum.GERMANY
 
-        case CountryEnum.BRAZIL:
-            return CityCountryEnum.BRAZIL;
+    case CountryEnum.BRAZIL:
+      return CityCountryEnum.BRAZIL
 
-        case CountryEnum.DENMARK:
-            return CityCountryEnum.DENMARK;
+    case CountryEnum.DENMARK:
+      return CityCountryEnum.DENMARK
 
-        case CountryEnum.COLOMBIA:
-            return CityCountryEnum.COLOMBIA;
+    case CountryEnum.COLOMBIA:
+      return CityCountryEnum.COLOMBIA
 
-        case CountryEnum.SPAIN:
-            return CityCountryEnum.SPAIN;
-    }
+    case CountryEnum.SPAIN:
+      return CityCountryEnum.SPAIN
+  }
 }
