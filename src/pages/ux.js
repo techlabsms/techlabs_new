856cf67704e4b37762e5d43af18f238ab11944f8
Paola Barboglio <paola.barboglio@gmail.com>
2020-08-11 15:53:22 +0200

    update ux component page with strings

--- src/pages/ux.js
+++ src/pages/ux.js
@@ -29,50 +29,48 @@ class ux extends Component {
       <Layout>
         <div className="container-fluid">
           <ProgrammHero
-            headingFirst="UX"
-            headingSecond=" Design"
+            headingFirst={<FormattedMessage id={"ux.headline.ProgrammHero.headingFirst"}/>}
+            headingSecond={<FormattedMessage id={"ux.headline.ProgrammHero.headingSecond"}/>}
             showCard={true}
             background={background}
-            intro="Inform yourself here about our UX Design track. Start your six months learning journey as a part of the next batch in the city of your choice. Acquire state-of-the-art tech skills by using one of our individualized tracks, local events, and professional mentoring. Within six months you will finish a tech project and be rewarded with our Digital Shaper certificate."
+            intro={<FormattedMessage id={"ux.headline.ProgrammHero.intro"}/>}
             backgroundMobil={backgroundMobil}
           />
         </div>
         <div className="container">
           <RightImageSection
-            heading="What is User Experience Design?"
-            subheading="A UX designer’s tasks often include user research, designing interactive prototypes, and testing designs."
-            text="The User Experience is made up of a person’s perception and response resulting from the use or even just the anticipated use of a product, system, or service. Therefore, UX Design is the process of understanding the user to iteratively improve the usability, accessibility and pleasure while interacting with the product. In short: Your job is it to make a user enjoy your product."
+            heading={<FormattedMessage id={"ux.what.RightImageSection.heading"}/>}
+            subheading={<FormattedMessage id={"ux.what.RightImageSection.subheading"}/>}
+            text={<FormattedMessage id={"ux.what.RightImageSection.text"}/>}
             image={UX2}
           />
           <RightImageSection
-            heading="Why User Experience Design?"
-            subheading="Every product has an user experience. Learn how to perfect it!"
-            text="Since companies started anticipating the value of creating a good user experience products in all life areas have become more and more user friendly. As a result the the demand for good usability and positive experiences grew over time. In addition to that there are new products entering the market every day, all of which have different demands to fulfill the needs of the people that use or even depend on them.
-                    UX Design is not only necessary to successfully establish a product it is also essential to keep the product on the market and develop it further.
-                    As a lot of products and informations are turning digital you will be the key to making them accessible for the people that depend on them and with that have the chance to make their lives easier. "
+            heading={<FormattedMessage id={"ux.why.RightImageSection.heading"}/>}
+            subheading={<FormattedMessage id={"ux.why.RightImageSection.subheading"}/>}
+            text={<FormattedMessage id={"ux.why.RightImageSection.text"}/>}
             image={UX}
           />
           <WhatYouWillLearn
-            intro="Define and scope the design problem you will try to address!"
-            first="Learn UX Design with practical examples and use the opportunity to build a personal portfolio."
-            second="Find out what the key values are for designing an enjoyable user experience"
-            third="Get experience in communicating in a multidisciplinary team and create your own design portfolio."
-            fourth="Get to know tools to prototype and implement your ideas."
+            intro={<FormattedMessage id={"ux.learn.WhatYouWillLearn.intro"}/>}
+            first={<FormattedMessage id={"ux.learn.WhatYouWillLearn.first"}/>}
+            second={<FormattedMessage id={"ux.learn.WhatYouWillLearn.second"}/>}
+            third={<FormattedMessage id={"ux.learn.WhatYouWillLearn.third"}/>}
+            fourth={<FormattedMessage id={"ux.learn.WhatYouWillLearn.fourth"}/>}
           />
           <LeftImageSection
-            heading="More about the Track"
-            subheading="Great design is born, nurtured, and grown – all through a systematic, learnable process!"
-            text="Within the track, you will explore the process of taking a basic concept, grounded in user needs, and developing it into a design that will address those needs. You will gain hands-on experience with techniques such as prototyping, scenario and persona development as well as wireframing that will help you transform your understanding of what your users need into a compelling user experience. Additionally, you turn your ideas into interactive prototypes that can be tested with prospective users and iteratively turned into a high quality design."
+            heading={<FormattedMessage id={"ux.more.LeftImageSection.heading"}/>} 
+            subheading={<FormattedMessage id={"ux.more.LeftImageSection.subheading"}/>}
+            text={<FormattedMessage id={"ux.more.LeftImageSection.text"}/>}
             image={david}
           />
           <Projects />
           <CallToAction />
           <FirstImpression
-            heading="Get a first Impression"
-            subheading="Check out our curriculum that is open for everybody!"
-            firstHeading="Introduction to User Experience Design"
+            heading={<FormattedMessage id={"ux.first_impression.FirstImpression.heading"}/>}
+            subheading={<FormattedMessage id={"ux.first_impression.FirstImpression.subheading"}/>}
+            firstHeading={<FormattedMessage id={"ux.first_impression.FirstImpression.firstHeading"}/>}
             firstImage={UX2}
-            firstText="Start with our free Track and learn the fundamentals now and see if it’s right for you"
+            firstText={<FormattedMessage id={"ux.first_impression.FirstImpression.firstText"}/>}
             firstLink="https://app.edyoucated.org/invitation/team/c6a5346d-035c-4a98-bf1b-13c36fe25eb3"
           />
            <Faq>
