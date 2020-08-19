06687d8c310977bcb548562127f91514405d98cd
Paola Barboglio <paola.barboglio@gmail.com>
2020-08-19 14:29:04 +0200

    add role paragraph and description to team component

--- src/components/locations/Team.js
+++ src/components/locations/Team.js
@@ -34,6 +34,8 @@ const Team = ({ city, team }) => {
               <h3 className="team--name text-center">
                 <span className="highlighted">{teamMember.name}</span>
               </h3>
+              <p>Role</p>
+              <p>Description</p>
               <div className="mt-4">
                 <a
                   href={teamMember.linkedIn}
