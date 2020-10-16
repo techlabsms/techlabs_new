9bc47e7724bad662175a6fdfd1f172500dc70877
Paola <paola.barboglio@gmail.com>
2020-10-16 15:33:56 +0200

    add Layout component to program/index and program/local, remove unnecessary imports

--- src/pages/program/index.js
+++ src/pages/program/index.js
@@ -5,6 +5,7 @@ import { Helmet } from "react-helmet"
 import { FormattedMessage } from "gatsby-plugin-intl"
 
 // components
+import Layout from "../../components/Layout/Layout"
 import Navbar from "../../components/Layout/Navbar"
 import Footer from "../../components/Layout/Footer"
 import Button from "../../components/smallComponents/Button"
@@ -25,102 +26,97 @@ const index = () => {
   return (
     <>
       <div>
-        <Navbar gaLabel="p"/>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Program | TechLabs - We Build. Digital. Shapers.</title>
-          <link rel="canonical" href="https://www.techlabs.org/program" />
-        </Helmet>
-        <ProgrammHero
-          headingFirst={<FormattedMessage id={"programs.heading.1"} />}
-          headingSecond={<FormattedMessage id={"programs.heading.2"} />}
-          intro={
-            <FormattedMessage id={"programs.subheading"} />
-          }
-          background={background}
-          backgroundMobil={ProgrammMobil}
-        />
-        <div className="container">
-          <Academy
-            page="p"
-            firstTrack={
-              <FormattedMessage id={"program.local.academy.first.name"} />
+        <Layout gaLabel="p" pageTitle="Program">
+          <ProgrammHero
+            headingFirst={<FormattedMessage id={"programs.heading.1"} />}
+            headingSecond={<FormattedMessage id={"programs.heading.2"} />}
+            intro={
+              <FormattedMessage id={"programs.subheading"} />
             }
-            firstTrackText={
-              <FormattedMessage id={"program.local.academy.first.text"} />
-            }
-            secondTrack={
-              <FormattedMessage id={"program.local.academy.second.name"} />
-            }
-            secondTrackText={
-              <FormattedMessage id={"program.local.academy.second.text"} />
-            }
-            thirdTrack={
-              <FormattedMessage id={"program.local.academy.third.name"} />
-            }
-            thirdTrackText={
-              <FormattedMessage id={"program.local.academy.third.text"} />
-            }
-            fourthTrack={
-              <FormattedMessage id={"program.local.academy.fourth.name"} />
-            }
-            fourthTrackText={
-              <FormattedMessage id={"program.local.academy.fourth.text"} />
-            }
+            background={background}
+            backgroundMobil={ProgrammMobil}
           />
-          <LearnMore
-            heading={
-              <FormattedMessage id={"landingpage.learn_more.heading"} />
-            } 
-            subheading={
-              <FormattedMessage id={"programs.learn_more.subheading"} />
-            } 
-            firstProjectHeading="Local Digital Shaper Program"
-            firstProjectImage={web}
-            firstLink="/program/local"
-            firstButtonText={
-              <FormattedMessage id={"programs.learn_more.button"}/>
-            }
-            secondProjectHeading="#codeathome Bootcamp"
-            secondProjectImage={DataScience}
-            secondLink="/program/remote"
-            secondButtonText={
-              <FormattedMessage id={"programs.learn_more.button"}/>
-            }
-            firstButtonSection="lm_ldsp"
-            secondButtonSection="lm_c@hb"
-            gaLocation="p"
-          />
-          <Faq>
-            <FaqQuestion
-              question="faq_1.question"
-              answer="faq_1.answer"
+          <div className="container">
+            <Academy
+              page="p"
+              firstTrack={
+                <FormattedMessage id={"program.local.academy.first.name"} />
+              }
+              firstTrackText={
+                <FormattedMessage id={"program.local.academy.first.text"} />
+              }
+              secondTrack={
+                <FormattedMessage id={"program.local.academy.second.name"} />
+              }
+              secondTrackText={
+                <FormattedMessage id={"program.local.academy.second.text"} />
+              }
+              thirdTrack={
+                <FormattedMessage id={"program.local.academy.third.name"} />
+              }
+              thirdTrackText={
+                <FormattedMessage id={"program.local.academy.third.text"} />
+              }
+              fourthTrack={
+                <FormattedMessage id={"program.local.academy.fourth.name"} />
+              }
+              fourthTrackText={
+                <FormattedMessage id={"program.local.academy.fourth.text"} />
+              }
             />
-            <FaqQuestion
-              question="faq_2.question"
-              answer="faq_2.answer"
+            <LearnMore
+              heading={
+                <FormattedMessage id={"landingpage.learn_more.heading"} />
+              } 
+              subheading={
+                <FormattedMessage id={"programs.learn_more.subheading"} />
+              } 
+              firstProjectHeading="Local Digital Shaper Program"
+              firstProjectImage={web}
+              firstLink="/program/local"
+              firstButtonText={
+                <FormattedMessage id={"programs.learn_more.button"}/>
+              }
+              secondProjectHeading="#codeathome Bootcamp"
+              secondProjectImage={DataScience}
+              secondLink="/program/remote"
+              secondButtonText={
+                <FormattedMessage id={"programs.learn_more.button"}/>
+              }
+              firstButtonSection="lm_ldsp"
+              secondButtonSection="lm_c@hb"
+              gaLocation="p"
             />
-            <FaqQuestion
-              question="faq_3.question"
-              answer="faq_3.answer"
-            />
-          </Faq>
-          <div className="container pb-5">
-            <div className="row">
-              <div className="col text-center">
-                <Button 
-                text={
-                  <FormattedMessage id={"faq.button"}/>
-                }
-                link="/faq" 
-                primary={true}
-                gaLocation="p"
-                gaSection="faq_mq" />
+            <Faq>
+              <FaqQuestion
+                question="faq_1.question"
+                answer="faq_1.answer"
+              />
+              <FaqQuestion
+                question="faq_2.question"
+                answer="faq_2.answer"
+              />
+              <FaqQuestion
+                question="faq_3.question"
+                answer="faq_3.answer"
+              />
+            </Faq>
+            <div className="container pb-5">
+              <div className="row">
+                <div className="col text-center">
+                  <Button 
+                  text={
+                    <FormattedMessage id={"faq.button"}/>
+                  }
+                  link="/faq" 
+                  primary={true}
+                  gaLocation="p"
+                  gaSection="faq_mq" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <Footer />
+        </Layout>
       </div>
     </>
   )
--- src/pages/program/local.js
+++ src/pages/program/local.js
@@ -6,8 +6,7 @@ import { Helmet } from "react-helmet"
 import { FormattedMessage } from "gatsby-plugin-intl"
 
 // components
-import Navbar from "../../components/Layout/Navbar"
-import Footer from "../../components/Layout/Footer"
+import Layout from "../../components/Layout/Layout"
 import Button from "../../components/smallComponents/Button"
 import Academy from "../../components/program/Academy"
 import ThreeComponents from "../../components/program/ThreeComponents"
@@ -36,194 +35,190 @@ class local extends Component {
     } = data.page.edges[0].node
     return (
       <div>
-        <Navbar gaLabel="ldsp"/>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Local | TechLabs - We Build. Digital. Shapers.</title>
-        </Helmet>
-        <ProgrammHero
-          headingFirst="Digital Shaper"
-          headingSecond={<FormattedMessage id={"program.local.programmhero.headingSecond"}/>}
-          intro={<FormattedMessage id={"programs.subheading"}/>}
-          background={background}
-          backgroundMobil={ProgrammMobil}
-        />
-        <div className="container">
-          <Academy
-            firstTrack={
-              <FormattedMessage id={"program.local.academy.first.name"} />
-            }
-            firstTrackText={
-              <FormattedMessage id={"program.local.academy.first.text"} />
-            }
-            secondTrack={
-              <FormattedMessage id={"program.local.academy.second.name"} />
-            }
-            secondTrackText={
-              <FormattedMessage id={"program.local.academy.second.text"} />
-            }
-            thirdTrack={
-              <FormattedMessage id={"program.local.academy.third.name"} />
-            }
-            thirdTrackText={
-              <FormattedMessage id={"program.local.academy.third.text"} />
-            }
-            fourthTrack={
-              <FormattedMessage id={"program.local.academy.fourth.name"} />
-            }
-            fourthTrackText={
-              <FormattedMessage id={"program.local.academy.fourth.text"} />
-            }
-            page="ldsp"
+        <Layout gaLabel="ldsp" pageTitle="Local">
+          <ProgrammHero
+            headingFirst="Digital Shaper"
+            headingSecond={<FormattedMessage id={"program.local.programmhero.headingSecond"}/>}
+            intro={<FormattedMessage id={"programs.subheading"}/>}
+            background={background}
+            backgroundMobil={ProgrammMobil}
           />
-          <Table
-            heading={<FormattedMessage id={"program.local.table.heading"} />}
-            subheading={
-              <FormattedMessage id={"program.local.table.subheading"} />
-            }
-            activeProgram="local"
-            hasButton={false}
-            codeathome={whatIsIncluded}
-          />
-          <ThreeComponents
-            heading={
-              <FormattedMessage id={"program.local.threeComponents.heading"} />
-            }
-            subheading={
-              <FormattedMessage
-                id={"program.local.threeComponents.subheading"}
-              />
-            }
-            firstHeading={
-              <FormattedMessage
-                id={"program.local.threeComponents.first.heading"}
-              />
-            }
-            firstText={
-              <FormattedMessage
-                id={"program.local.threeComponents.first.text"}
-              />
-            }
-            secondHeading={
-              <FormattedMessage
-                id={"program.local.threeComponents.second.heading"}
-              />
-            }
-            secondText={
-              <FormattedMessage
-                id={"program.local.threeComponents.second.text"}
-              />
-            }
-            thirdHeading={
-              <FormattedMessage
-                id={"program.local.threeComponents.third.heading"}
-              />
-            }
-            thirdText={
-              <FormattedMessage
-                id={"program.local.threeComponents.third.text"}
-              />
-            }
-          />
-          <KeyBenefits
-            heading={
-              <FormattedMessage id={"program.local.keyBenefits.heading"} />
-            }
-            subheading={
-              <FormattedMessage id={"program.local.keyBenefits.subheading"} />
-            }
-            firstHeading={
-              <FormattedMessage
-                id={"program.local.keyBenefits.first.heading"}
-              />
-            }
-            firstText={
-              <FormattedMessage id={"program.local.keyBenefits.first.text"} />
-            }
-            secondHeading={
-              <FormattedMessage
-                id={"program.local.keyBenefits.second.heading"}
-              />
-            }
-            secondText={
-              <FormattedMessage id={"program.local.keyBenefits.second.text"} />
-            }
-            thirdHeading={
-              <FormattedMessage
-                id={"program.local.keyBenefits.third.heading"}
-              />
-            }
-            thirdText={
-              <FormattedMessage id={"program.local.keyBenefits.third.text"} />
-            }
-            fourthHeading={
-              <FormattedMessage
-                id={"program.local.keyBenefits.fourth.heading"}
-              />
-            }
-            fourthText={
-              <FormattedMessage id={"program.local.keyBenefits.fourth.text"} />
-            }
-          />
-          <Testimonial
-            heading={<FormattedMessage id={"testimonial.heading"}/>}
-            subheading={<FormattedMessage id={"program.local.testimonial.subheading"}/>}
-            text={<FormattedMessage id={"program.local.timothee.quote"}/>}
-            testimonialAvatar={data.timothee.childImageSharp.fluid}
-            testimonialName="Timothée Clolus"
-            testimonialTagline="Front end Developer @"
-            testimonialLogo={data.accenture.childImageSharp.fluid}
-          />
-          <Process />
-          <LearnMore
-            heading={<FormattedMessage id={"program.local.learn_more.heading"}/>}
-            subheading={<FormattedMessage id={"program.local.learn_more.subheading"}/>}
-            firstProjectHeading={<FormattedMessage id={"program.local.learn_more.firstProject"}/>}
-            firstProjectImage={web}
-            firstLink="/web"
-            secondProjectHeading={<FormattedMessage id={"program.local.learn_more.secondProject"}/>}
-            secondProjectImage={DataScience}
-            secondLink="/dataScience"
-            thirdProjectHeading={<FormattedMessage id={"program.local.learn_more.thirdProject"}/>}
-            thirdProjectImage={AI}
-            thirdLink="/ai"
-            fourthProjectHeading={<FormattedMessage id={"program.local.learn_more.fourthProject"}/>}
-            fourthProjectImage={UX}
-            fourthLink="/ux"
-            firstButtonSection="lm_wd"
-            secondButtonSection="lm_ds"
-            thirdButtonSection="lm_ai"
-            fourthButtonSection="lm_ux"
-            gaLocation="ldsp"
-          />
-          <Faq>
-            <FaqQuestion
-              question="faq_1.question"
-              answer="faq_1.answer"
+          <div className="container">
+            <Academy
+              firstTrack={
+                <FormattedMessage id={"program.local.academy.first.name"} />
+              }
+              firstTrackText={
+                <FormattedMessage id={"program.local.academy.first.text"} />
+              }
+              secondTrack={
+                <FormattedMessage id={"program.local.academy.second.name"} />
+              }
+              secondTrackText={
+                <FormattedMessage id={"program.local.academy.second.text"} />
+              }
+              thirdTrack={
+                <FormattedMessage id={"program.local.academy.third.name"} />
+              }
+              thirdTrackText={
+                <FormattedMessage id={"program.local.academy.third.text"} />
+              }
+              fourthTrack={
+                <FormattedMessage id={"program.local.academy.fourth.name"} />
+              }
+              fourthTrackText={
+                <FormattedMessage id={"program.local.academy.fourth.text"} />
+              }
+              page="ldsp"
             />
-            <FaqQuestion
-              question="faq_2.question"
-              answer="faq_2.answer"
+            <Table
+              heading={<FormattedMessage id={"program.local.table.heading"} />}
+              subheading={
+                <FormattedMessage id={"program.local.table.subheading"} />
+              }
+              activeProgram="local"
+              hasButton={false}
+              codeathome={whatIsIncluded}
             />
-            <FaqQuestion
-              question="faq_3.question"
-              answer="faq_3.answer"
+            <ThreeComponents
+              heading={
+                <FormattedMessage id={"program.local.threeComponents.heading"} />
+              }
+              subheading={
+                <FormattedMessage
+                  id={"program.local.threeComponents.subheading"}
+                />
+              }
+              firstHeading={
+                <FormattedMessage
+                  id={"program.local.threeComponents.first.heading"}
+                />
+              }
+              firstText={
+                <FormattedMessage
+                  id={"program.local.threeComponents.first.text"}
+                />
+              }
+              secondHeading={
+                <FormattedMessage
+                  id={"program.local.threeComponents.second.heading"}
+                />
+              }
+              secondText={
+                <FormattedMessage
+                  id={"program.local.threeComponents.second.text"}
+                />
+              }
+              thirdHeading={
+                <FormattedMessage
+                  id={"program.local.threeComponents.third.heading"}
+                />
+              }
+              thirdText={
+                <FormattedMessage
+                  id={"program.local.threeComponents.third.text"}
+                />
+              }
             />
-          </Faq>
-          <div className="container pb-5">
-            <div className="row">
-              <div className="col text-center">
-                <Button 
-                  text={<FormattedMessage id={"faq.button"}/>}
-                  link="/faq" 
-                  primary={true}
-                  gaLocation="ldsp"
-                  gaSection="faq_mq" />
+            <KeyBenefits
+              heading={
+                <FormattedMessage id={"program.local.keyBenefits.heading"} />
+              }
+              subheading={
+                <FormattedMessage id={"program.local.keyBenefits.subheading"} />
+              }
+              firstHeading={
+                <FormattedMessage
+                  id={"program.local.keyBenefits.first.heading"}
+                />
+              }
+              firstText={
+                <FormattedMessage id={"program.local.keyBenefits.first.text"} />
+              }
+              secondHeading={
+                <FormattedMessage
+                  id={"program.local.keyBenefits.second.heading"}
+                />
+              }
+              secondText={
+                <FormattedMessage id={"program.local.keyBenefits.second.text"} />
+              }
+              thirdHeading={
+                <FormattedMessage
+                  id={"program.local.keyBenefits.third.heading"}
+                />
+              }
+              thirdText={
+                <FormattedMessage id={"program.local.keyBenefits.third.text"} />
+              }
+              fourthHeading={
+                <FormattedMessage
+                  id={"program.local.keyBenefits.fourth.heading"}
+                />
+              }
+              fourthText={
+                <FormattedMessage id={"program.local.keyBenefits.fourth.text"} />
+              }
+            />
+            <Testimonial
+              heading={<FormattedMessage id={"testimonial.heading"}/>}
+              subheading={<FormattedMessage id={"program.local.testimonial.subheading"}/>}
+              text={<FormattedMessage id={"program.local.timothee.quote"}/>}
+              testimonialAvatar={data.timothee.childImageSharp.fluid}
+              testimonialName="Timothée Clolus"
+              testimonialTagline="Front end Developer @"
+              testimonialLogo={data.accenture.childImageSharp.fluid}
+            />
+            <Process />
+            <LearnMore
+              heading={<FormattedMessage id={"program.local.learn_more.heading"}/>}
+              subheading={<FormattedMessage id={"program.local.learn_more.subheading"}/>}
+              firstProjectHeading={<FormattedMessage id={"program.local.learn_more.firstProject"}/>}
+              firstProjectImage={web}
+              firstLink="/web"
+              secondProjectHeading={<FormattedMessage id={"program.local.learn_more.secondProject"}/>}
+              secondProjectImage={DataScience}
+              secondLink="/dataScience"
+              thirdProjectHeading={<FormattedMessage id={"program.local.learn_more.thirdProject"}/>}
+              thirdProjectImage={AI}
+              thirdLink="/ai"
+              fourthProjectHeading={<FormattedMessage id={"program.local.learn_more.fourthProject"}/>}
+              fourthProjectImage={UX}
+              fourthLink="/ux"
+              firstButtonSection="lm_wd"
+              secondButtonSection="lm_ds"
+              thirdButtonSection="lm_ai"
+              fourthButtonSection="lm_ux"
+              gaLocation="ldsp"
+            />
+            <Faq>
+              <FaqQuestion
+                question="faq_1.question"
+                answer="faq_1.answer"
+              />
+              <FaqQuestion
+                question="faq_2.question"
+                answer="faq_2.answer"
+              />
+              <FaqQuestion
+                question="faq_3.question"
+                answer="faq_3.answer"
+              />
+            </Faq>
+            <div className="container pb-5">
+              <div className="row">
+                <div className="col text-center">
+                  <Button 
+                    text={<FormattedMessage id={"faq.button"}/>}
+                    link="/faq" 
+                    primary={true}
+                    gaLocation="ldsp"
+                    gaSection="faq_mq" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <Footer />
+        </Layout>
       </div>
     )
   }
