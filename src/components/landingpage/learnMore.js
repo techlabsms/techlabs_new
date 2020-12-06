a8952dc91e76e9e0bc806166b3bf6bc9297f9095
Richard <richardmenning@googlemail.com>
2020-12-06 16:39:22 +0100

    learn more section as own component

---
+++ src/components/landingpage/learnMore.js
@@ -0,0 +1,74 @@
+import React from "react"
+import BackgroundImage from "gatsby-background-image"
+import { FormattedMessage, Link } from "gatsby-plugin-intl"
+
+import Container from "../smallComponents/Container"
+import Heading from "../smallComponents/Heading"
+
+const LearnMore = ({ backgroundImage }) => {
+  return (
+    <section className="container-fluid mt-5 background h-100 py-5">
+      <BackgroundImage
+        fluid={backgroundImage}
+        style={{
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <Container>
+          <Heading
+            heading={<FormattedMessage id={"landingpage.learn_more.heading"} />}
+            subheading={
+              <FormattedMessage id={"landingpage.learn_more.subheading"} />
+            }
+          />
+          <div className="row h-100">
+            <div className="col-md-6 mt-3">
+              <Link to="/locations" className="noDec">
+                <div className="card">
+                  <h2 className="mt-3 color-red">
+                    {
+                      <FormattedMessage
+                        id={"landingpage.learn_more.locations.h2"}
+                      />
+                    }
+                  </h2>
+                  <p className="color-gl">
+                    {
+                      <FormattedMessage
+                        id={"landingpage.learn_more.locations.text"}
+                      />
+                    }
+                  </p>
+                </div>
+              </Link>
+            </div>
+            <div className="col-md-6 mt-3">
+              <Link to="/program" className="noDec">
+                <div className="card">
+                  <h2 className="mt-3 color-red">
+                    {
+                      <FormattedMessage
+                        id={"landingpage.learn_more.program.h2"}
+                      />
+                    }
+                  </h2>
+                  <p className="color-gl">
+                    {
+                      <FormattedMessage
+                        id={"landingpage.learn_more.program.text"}
+                      />
+                    }
+                  </p>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </Container>
+      </BackgroundImage>
+    </section>
+  )
+}
+
+export default LearnMore
--- src/pages/index.js
+++ src/pages/index.js
@@ -24,6 +24,7 @@ import GIC from "../assets/gic.svg"
 // styles
 import "../styles/_main.scss"
 import "../styles/bootstrap.min.css"
+import LearnMore from "../components/landingpage/learnMore"
 
 class index extends React.Component {
   componentDidMount() {
@@ -367,71 +368,9 @@ class index extends React.Component {
               <FormattedMessage id={"program.local.keyBenefits.fourth.text"} />
             }
           />
-          <section className="container-fluid mt-5 background h-100 py-5">
-            <BackgroundImage
-              fluid={data.background_location.childImageSharp.fluid}
-              style={{
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-              }}
-            >
-              <Container>
-                <Heading
-                  heading={
-                    <FormattedMessage id={"landingpage.learn_more.heading"} />
-                  }
-                  subheading={
-                    <FormattedMessage
-                      id={"landingpage.learn_more.subheading"}
-                    />
-                  }
-                />
-                <div className="row h-100">
-                  <div className="col-md-6 mt-3">
-                    <Link to="/locations" className="noDec">
-                      <div className="card">
-                        <h2 className="mt-3 color-red">
-                          {
-                            <FormattedMessage
-                              id={"landingpage.learn_more.locations.h2"}
-                            />
-                          }
-                        </h2>
-                        <p className="color-gl">
-                          {
-                            <FormattedMessage
-                              id={"landingpage.learn_more.locations.text"}
-                            />
-                          }
-                        </p>
-                      </div>
-                    </Link>
-                  </div>
-                  <div className="col-md-6 mt-3">
-                    <Link to="/program" className="noDec">
-                      <div className="card">
-                        <h2 className="mt-3 color-red">
-                          {
-                            <FormattedMessage
-                              id={"landingpage.learn_more.program.h2"}
-                            />
-                          }
-                        </h2>
-                        <p className="color-gl">
-                          {
-                            <FormattedMessage
-                              id={"landingpage.learn_more.program.text"}
-                            />
-                          }
-                        </p>
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-              </Container>
-            </BackgroundImage>
-          </section>
+          <LearnMore
+            backgroundImage={data.background_location.childImageSharp.fluid}
+          />
         </div>
       </Layout>
     )
