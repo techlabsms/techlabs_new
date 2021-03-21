9e57bc77d96ddc72f20ab01d12a6960648a9517a
Richard <richardmenning@googlemail.com>
2021-03-21 17:52:03 +0100

    build new apply page

---
+++ src/components/applyPage/ApplyCheckout.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+
+const ApplyCheckout = ({ stepOne, stepTwo, stepThree }) => {
+  const [currentStep, setCurrentStep] = useState(1)
+
+  const getCurrentStep = num => {
+    if (num === currentStep) {
+      return "c-tracker__num--centered"
+    } else if (num > currentStep) {
+      return "c-tracker__num--bottom"
+    } else {
+      return "c-tracker__num--top"
+    }
+  }
+
+  const handleClick = num => {
+    document.getElementById(num).classList = "c-tracker__animation"
+    setTimeout(() => {
+      setCurrentStep(currentStep + 1)
+    }, 500)
+  }
+  return (
+    <section className="c-checkout">
+      <div className="c-tracker">
+        <h5 className={`c-tracker__num ${getCurrentStep(1)}`}>1</h5>
+        <h5 className={`c-tracker__num ${getCurrentStep(2)}`}>2</h5>
+        <h5 className={`c-tracker__num ${getCurrentStep(3)}`}>3</h5>
+      </div>
+      {currentStep === 1 ? <div id="1">{stepOne}</div> : null}
+      {currentStep === 2 ? (
+        <div id="2" className="c-tracker__animation--up">
+          {stepTwo}
+        </div>
+      ) : null}
+      {currentStep === 3 ? (
+        <div id="3" className="c-tracker__animation--up">
+          {stepThree}
+        </div>
+      ) : null}
+      <div className="container mt-5">
+        <button onClick={() => handleClick(currentStep)}>Next</button>
+      </div>
+    </section>
+  )
+}
+
+export default ApplyCheckout
---
+++ src/pages/new-apply.js
@@ -0,0 +1,186 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import { graphql } from "gatsby"
+
+// plugins & external
+import { injectIntl, FormattedMessage } from "gatsby-plugin-intl"
+
+// components
+import Layout from "../components/Layout/Layout"
+import Heading from "../components/smallComponents/Heading"
+import ApplicationHero from "../components/applyPage/ApplicationHero"
+import ApplyCheckout from "../components/applyPage/ApplyCheckout"
+import ChooseCity from "../components/applyPage/ChooseCity"
+import TypeFormEmbedd from "../components/applyPage/TypeformEbedd"
+
+const NewApply = props => {
+  const [value, setValue] = useState({
+    isClicked: false,
+    cityValue: "",
+    link: "",
+    available: false,
+  })
+  const allCountries = props.data.allContentfulLocationPage.edges
+  const { intl } = props
+  return (
+    <Layout>
+      <ApplicationHero
+        heading="Application"
+        subheading="Apply now for one of our Bootcamps"
+        text="Do you want to learn state-of-the-art tech? Do you want to be part
+        of a thriving community? Apply now for the TechLabs Digital Shaper
+        Program. This is how the application works:"
+      />
+
+      <section className="c-explainer container">
+        <div className="c-explainer__layout">
+          <div className="c-explainer__number c-explainer__number--one">
+            <div className="c-explainer__circle">1</div>
+            <div className="c-explainer__circle--background"></div>
+          </div>
+          <div className="c-explainer__line">
+            <div className="c-explainer__line--connector "></div>
+          </div>
+          <div className="c-explainer__number c-explainer__number--two">
+            <div className="c-explainer__circle">2</div>
+            <div className="c-explainer__circle--background"></div>
+          </div>
+          <div className="c-explainer__line">
+            <div className="c-explainer__line--connector"></div>
+          </div>
+          <div className="c-explainer__number c-explainer__number--three">
+            <div className="c-explainer__circle">3</div>
+            <div className="c-explainer__circle--background"></div>
+          </div>
+        </div>
+        <div className="c-explainer__layout">
+          <div className="c-explainer__text c-explainer__text--one">
+            <h5>Choose Track and City</h5>
+            <p>
+              Inform yourself about our program and the 4 available tracks.
+              Choose one you want to apply for.
+              <br />
+              <Link to="/program">More about the tracks</Link>
+            </p>
+          </div>
+          <div className="c-explainer__text c-explainer__text--two">
+            <h5>Online Application</h5>
+            <p>
+              Apply online! We look through your application and check whether
+              your motivation fits our requirements!
+            </p>
+          </div>
+          <div className="c-explainer__text c-explainer__text--three">
+            <h5>Feedback Process</h5>
+            <p>
+              After looking through all applications, we quickly get back to you
+              with our final feedback!
+            </p>
+          </div>
+        </div>
+      </section>
+      <ApplyCheckout
+        stepOne={
+          <div className="container">
+            <Heading
+              heading="Requirements"
+              subheading="What we are looking for in an applicant"
+            />
+            <div className="row mt-3">
+              <div className="col-md-6">
+                <h5>
+                  <span className="highlighted">Motivation</span>
+                </h5>
+                <p>
+                  Are you motivated to learn tech skills online? The online
+                  learning program requires you to learn independently!
+                </p>
+              </div>
+              <div className="col-md-6">
+                <span className="highlighted">Motivation</span>
+                <p>
+                  Are you motivated to learn tech skills online? The online
+                  learning program requires you to learn independently!
+                </p>
+              </div>
+            </div>
+            <div className="row mt-3">
+              <div className="col-md-6">
+                <span className="highlighted">Motivation</span>
+                <p>
+                  Are you motivated to learn tech skills online? The online
+                  learning program requires you to learn independently!
+                </p>
+              </div>
+              <div className="col-md-6">
+                <span className="highlighted">Motivation</span>
+                <p>
+                  Are you motivated to learn tech skills online? The online
+                  learning program requires you to learn independently!
+                </p>
+              </div>
+            </div>
+            <div className="row mt-3">
+              <div className="col-md-6">
+                <span className="highlighted">Motivation</span>
+                <p>
+                  Are you motivated to learn tech skills online? The online
+                  learning program requires you to learn independently!
+                </p>
+              </div>
+            </div>
+          </div>
+        }
+        stepTwo={
+          <ChooseCity
+            heading={
+              <FormattedMessage id={"applypage.choose.your.city.title"} />
+            }
+            subheading={
+              <FormattedMessage
+                id={"applypage.choose.your.city.subtitle"}
+                values={{ count: allCountries.length - 1 }}
+              />
+            }
+            locale={intl.locale}
+            handleClick={(isClicked, value, available, link) => {
+              setValue({
+                isClicked,
+                cityValue: value,
+                available,
+                link,
+              })
+            }}
+            clickedCityValue={value}
+            countries={allCountries}
+          />
+        }
+        stepThree={<TypeFormEmbedd />}
+      ></ApplyCheckout>
+    </Layout>
+  )
+}
+
+export default injectIntl(NewApply)
+
+export const pageQuery = graphql`
+  query($locale: String) {
+    allContentfulLocationPage(filter: { node_locale: { eq: $locale } }) {
+      edges {
+        node {
+          heading
+          country
+          applicationStart
+          applicationEnd
+          applicationLink
+          avaiableTracks {
+            ai
+            web
+            data
+            ux
+          }
+        }
+      }
+    }
+  }
+`
