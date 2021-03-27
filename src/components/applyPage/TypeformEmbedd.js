fee5654d8a013c0bc2be5bff3cec9928801e18be
Richard <richardmenning@googlemail.com>
2021-03-27 09:51:58 +0100

    add border to typefrom

--- src/components/applyPage/TypeformEmbedd.js
+++ src/components/applyPage/TypeformEmbedd.js
@@ -4,21 +4,24 @@ import Heading from "../smallComponents/Heading"
 
 const TypeformEmbedd = ({ url }) => {
   return (
-    <div className="container relative">
+    <div className="container relative c-typformEmbedd">
       <Heading
         heading="Requirements"
         subheading="What we are looking for in an applicant"
       />
+
       <ReactTypeformEmbed
         url={url}
         style={{
           width: "90%",
           height: "80%",
-          top: "20%",
+          top: "10%",
           position: "absolute",
           left: "50%",
           transform: "translateX(-50%)",
           maxWidth: "1140px",
+          border: "1px solid #fa1e5a",
+          borderRadius: "5px",
         }}
       ></ReactTypeformEmbed>
     </div>
