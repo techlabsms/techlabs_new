bb4517d3450886aa8df1f0fe75955aace4e47a03
Richard <richardmenning@googlemail.com>
2021-04-08 07:21:59 +0200

    cherry-picked from new apply page

--- src/components/applyPage/TypeformEmbedd.js
+++ src/components/applyPage/TypeformEmbedd.js
@@ -2,25 +2,11 @@ import React from "react"
 import { ReactTypeformEmbed } from "react-typeform-embed"
 import Heading from "../smallComponents/Heading"
 
-const TypeformEmbedd = ({ url }) => {
+const TypeformEmbedd = ({ url, heading, subheading, style }) => {
   return (
     <div className="container relative">
-      <Heading
-        heading="Requirements"
-        subheading="What we are looking for in an applicant"
-      />
-      <ReactTypeformEmbed
-        url={url}
-        style={{
-          width: "90%",
-          height: "80%",
-          top: "20%",
-          position: "absolute",
-          left: "50%",
-          transform: "translateX(-50%)",
-          maxWidth: "1140px",
-        }}
-      ></ReactTypeformEmbed>
+      <Heading heading={heading} subheading={subheading} />
+      <ReactTypeformEmbed url={url} style={style}></ReactTypeformEmbed>
     </div>
   )
 }
