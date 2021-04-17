f23d2e9a6ae59e741d517babdac526e8876a4b31
Richard <richardmenning@googlemail.com>
2021-04-17 16:22:23 +0200

    open in a new tab button

--- src/components/applyPage/TypeformEmbedd.js
+++ src/components/applyPage/TypeformEmbedd.js
@@ -10,12 +10,11 @@ const TypeformEmbedd = ({ url, isMobile }) => {
         subheading="Ready to apply? Please fill in the form below."
       />
 
-      <ReactTypeformEmbed
-        url={url}
+      <div
         style={{
           width: isMobile ? "95%" : "90%",
           height: isMobile ? "90%" : "80%",
-          top: isMobile ? "30%" : "10%",
+          top: isMobile ? "30%" : "150px",
           position: "absolute",
           left: "50%",
           transform: "translateX(-50%)",
@@ -23,7 +22,25 @@ const TypeformEmbedd = ({ url, isMobile }) => {
           border: "1px solid #fa1e5a",
           borderRadius: "5px",
         }}
-      ></ReactTypeformEmbed>
+      >
+        <ReactTypeformEmbed url={url}></ReactTypeformEmbed>
+        <p
+          style={{
+            position: "relative",
+            top: "105%",
+          }}
+        >
+          Open the form in a new Tab{" "}
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="noDec navLink--active"
+          >
+            Open Typeform
+          </a>
+        </p>
+      </div>
     </div>
   )
 }
