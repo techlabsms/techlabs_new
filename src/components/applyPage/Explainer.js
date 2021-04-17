32dccae2a56aafed209f422b74b5a5e1eef42f1a
Pia <pia.schuermann@techlabs.org>
2021-04-17 12:15:16 +0200

    change of link color from blue to pink

--- src/components/applyPage/Explainer.js
+++ src/components/applyPage/Explainer.js
@@ -102,9 +102,9 @@ const Explainer = () => {
             <p>
               <FormattedMessage id={"applypage.steps.first.text"} />
               <br />
-              <Link to="/program">
-                <FormattedMessage id={"applypage.steps.first.link"} />
-              </Link>
+                <Link to="/program">
+                  <FormattedMessage id={"applypage.steps.first.link"} />
+                </Link>
             </p>
           </div>
           <div className="c-explainer__text c-explainer__text--two">
