a4c906306e2ce10a478f9a47b182835d07dff5e0
Richard <richardmenning@googlemail.com>
2021-04-26 16:59:53 +0200

    fix typeform on mobile

--- src/components/applyPage/TypeformEmbedd.js
+++ src/components/applyPage/TypeformEmbedd.js
@@ -4,42 +4,41 @@ import Heading from "../smallComponents/Heading"
 
 const TypeformEmbedd = ({ url, isMobile, heading, subheading }) => {
   return (
-    <div className="container relative c-typformEmbedd">
-      <Heading
-        heading={heading}
-        subheading={subheading}
-      />
+    <div className="pt-5 mt-3">
+      <div className="container relative c-typformEmbedd">
+        <Heading heading={heading} subheading={subheading} />
 
-      <div
-        style={{
-          width: isMobile ? "95%" : "90%",
-          height: isMobile ? "90%" : "80%",
-          top: isMobile ? "30%" : "150px",
-          position: "absolute",
-          left: "50%",
-          transform: "translateX(-50%)",
-          maxWidth: "1140px",
-          border: "1px solid #fa1e5a",
-          borderRadius: "5px",
-        }}
-      >
-        <ReactTypeformEmbed url={url}></ReactTypeformEmbed>
-        <p
+        <div
           style={{
-            position: "relative",
-            top: "105%",
+            width: isMobile ? "95%" : "90%",
+            height: isMobile ? "75%" : "80%",
+            top: isMobile ? "300px" : "220px",
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+            maxWidth: "1140px",
+            border: "1px solid #fa1e5a",
+            borderRadius: "5px",
           }}
         >
-          Open the form in a new Tab{" "}
-          <a
-            href={url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="noDec navLink--active"
+          <ReactTypeformEmbed url={url}></ReactTypeformEmbed>
+          <p
+            style={{
+              position: "relative",
+              top: "105%",
+            }}
           >
-            Open Typeform
-          </a>
-        </p>
+            Open the form in a new Tab{" "}
+            <a
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="noDec navLink--active"
+            >
+              Open Typeform
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   )
