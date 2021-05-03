6cdd82f88303771070ed8d2895ae8d3a118e2015
Richard <richardmenning@googlemail.com>
2021-05-03 22:10:21 +0200

    sort the open locations to the start

--- src/components/applyPage/ChooseCity.js
+++ src/components/applyPage/ChooseCity.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react"
 import ApplyCard from "./ApplyCard"
 import Dropdown from "../shared/dropdown"
 import Heading from "../smallComponents/Heading"
+import dayjs from "dayjs"
 
 // assets
 
@@ -23,6 +24,7 @@ const ChooseCity = ({
   const [countryOptions, setCountryOptions] = useState([])
   const [choosenCountry, setChoosenCountry] = useState("Brazil")
   const [windowWidth, setWindowWidth] = useState(width)
+  let now = dayjs(Date.now())
 
   const handleResize = () => {
     setWindowWidth(window.innerWidth)
@@ -34,6 +36,20 @@ const ChooseCity = ({
       window.removeEventListener("resize", handleResize)
     }
   })
+
+  const isCurrentlyOpen = location => {
+    if (location.node.applicationStart === null) {
+      return false
+    } else {
+      if (now.isAfter(dayjs(location.node.applicationStart))) {
+        if (now.isAfter(dayjs(location.node.applicationEnd))) {
+          return false
+        }
+      } else {
+        return true
+      }
+    }
+  }
 
   countries.forEach(c => {
     const { country } = c.node
@@ -52,6 +68,16 @@ const ChooseCity = ({
     return locations.node.country.toLowerCase() === choosenCountry.toLowerCase()
   })
 
+  const sortedCountries = filteredCountries.sort((a, b) => {
+    if (isCurrentlyOpen(a) && isCurrentlyOpen(b)) {
+      return -1
+    } else if (isCurrentlyOpen(a)) {
+      return -1
+    } else {
+      return 1
+    }
+  })
+
   return (
     <div className="container pt-5 mt-3">
       <Heading heading={heading} subheading={subheading} />
@@ -61,7 +87,7 @@ const ChooseCity = ({
         onSelect={option => setChoosenCountry(option)}
       />
       <div className="row">
-        {filteredCountries.map(country => (
+        {sortedCountries.map(country => (
           <ApplyCard
             cityValues={country.node}
             key={country.node.heading}
