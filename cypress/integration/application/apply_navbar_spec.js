75e5be1e5f4d3c23708822e87edf9673bd3ee908
Paola_Barboglio <paola.barboglio@gmail.com>
2021-05-16 11:01:56 +0200

    go to apply page via navbar

--- cypress/integration/application/apply_navbar_spec.js
+++ cypress/integration/application/apply_navbar_spec.js
@@ -1,21 +1,31 @@
 describe('Apply Now / Navbar flow', () => {
     it('Correctly navigates to application page, clicks on Aachen and displays apply button(s)', () => {       
-    // open homepage
-    cy.visit('http://localhost:8000/')
-    // close cookies consent banner
-    cy.get('button').contains('I understand').click()
-    // click on "Apply Now" button in the navbar
-    cy.get('.nav-button').contains('Apply now').click()
-    // Url should contain "/apply"
-    cy.url().should('include', '/apply')
-    // select dropdown Country -> Germany
-    cy.get('.dropdown-select').select('Germany')
-    // click on "Aachen" Apply Card
-    cy.get('[href="#requirements"]').contains('Aachen').click()
-    // if open, btn apply, should be enabled (or else, disabled)
-    cy.get('.btn-primary').contains('Application currently closed').should('be.disabled')
-    cy.get('.btn-primary').contains('Apply now').should('not.be.disabled')
+    cy.visit("/")
+    cy.get('.cookieConsent').children('button').first().click()
+    let countriesCount = 0
+
+    const navItems = cy.get('.navbar').children('#navigations-08').children().children()
+    navItems.should('have.length', 5)
+
+    navItems.children().last().click()
+    
+    cy.url().should('include', 'apply')
+
+    const explainerTexts = cy.get('.c-explainer__text')
+    explainerTexts.should('have.length', 3)
+    explainerTexts.first().children('p').contains('a').should('have.attr', 'href').and('match', /\/program/)
+
+    cy.get('.c-tracker__btn').click()
+    let index = Math.floor(Math.random() * 9)
+    cy.get('.dropdown-select').children().should('have.length', 9)
+    cy.get('.dropdown-select option').eq(index).invoke('val').then((value) => {
+      cy.get('.dropdown-select').select(value)
+    })
+
+    const cards = cy.get('.c-tracker__animation--up').children().children('.row').last().children()
+    cards.each(($card) => {
+      cy.wrap($card).find('a').should('have.attr', 'href').and('match', /#requirements/)
+    })
+
     })
   })
-
-  // MAKE DYNAMIC!!
