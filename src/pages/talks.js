6bdbb2c5d1e509ca65c46c458e75ce28257d874f
Richard <richardmenning@googlemail.com>
2021-06-06 19:16:54 +0200

    feat: filter for the language of the posts

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -13,49 +13,52 @@ import TalksCard from "../components/talks/TalkCard"
 import "../styles/_main.scss"
 
 const talks = props => {
-    const talks = props.data.allContentfulTalksPage.edges;
-    return(
-        <Layout>
-            <div className="talks-container">
-                <TalksHero/>
-                <div className="talks-searchbar">
-                    <h4><FormattedMessage id="talks.previous"/> ({talks.length})</h4>
-                    <input 
-                        placeholder="Search"
-                        onFocus={(e) => e.target.placeholder = ""} 
-                        onBlur={(e) => e.target.placeholder = "Search"}
-                    ></input>
-                </div>
-                <div className="talks-cards">
-                    {talks.map(talk => {
-                        return(
-                            <Link to={talk.node.slug}>
-                                <TalksCard
-                                title={talk.node.subtitle}
-                                speakers={talk.node.speakers}/>
-                            </Link>
-                        )                    
-                    })}
-                </div>
-            </div>
-        </Layout>
-    )
+  const talks = props.data.allContentfulTalksPage.edges
+  return (
+    <Layout>
+      <div className="talks-container">
+        <TalksHero />
+        <div className="talks-searchbar">
+          <h4>
+            <FormattedMessage id="talks.previous" /> ({talks.length})
+          </h4>
+          <input
+            placeholder="Search"
+            onFocus={e => (e.target.placeholder = "")}
+            onBlur={e => (e.target.placeholder = "Search")}
+          ></input>
+        </div>
+        <div className="talks-cards">
+          {talks.map(talk => {
+            return (
+              <Link to={talk.node.slug}>
+                <TalksCard
+                  title={talk.node.subtitle}
+                  speakers={talk.node.speakers}
+                />
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default injectIntl(talks)
 
 export const pageQuery = graphql`
-  query TalksPageQuery {
-    allContentfulTalksPage {
-        edges {
-          node {
-            subtitle
-            slug
-            speakers {
-              company
-            }
+  query($locale: String) {
+    allContentfulTalksPage(filter: { node_locale: { eq: $locale } }) {
+      edges {
+        node {
+          subtitle
+          slug
+          speakers {
+            company
           }
         }
       }
+    }
   }
 `
