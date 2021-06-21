3710b91c396755afc0c30fcfcad30f0cc0a02b70
Richard <richardmenning@googlemail.com>
2021-06-21 17:04:54 +0200

    update path

--- src/pages/talks/index.js
+++ src/pages/talks/index.js
@@ -5,7 +5,7 @@ import Layout from "../../components/Layout/Layout"
 const Index = ({ data }) => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      navigate("logistics")
+      navigate("neuroscience")
     }
   })
   return <Layout></Layout>
