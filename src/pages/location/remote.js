3537a01bde9268c0170d88c3816590e094ecfc62
RichardMEN11 <richardmenning@googlemail.com>
2023-10-23 19:44:12 +0200

    feat: :sparkles: add redirect for remote

---
+++ src/pages/location/remote.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import { Redirect } from "@reach/router"
+
+class Dortmund extends Component {
+  render() {
+    return <Redirect noThrow to="https://www.codeathome.techlabs.org/" />
+  }
+}
+
+export default Dortmund
